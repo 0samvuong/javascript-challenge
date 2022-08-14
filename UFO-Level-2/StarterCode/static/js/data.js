2700a3b6f94bc6736b1bf9f274e35f71faa588f1
0samvuong <101376900+0samvuong@users.noreply.github.com>
2022-08-14 19:35:03 +1000

    a

--- UFO-Level-2/StarterCode/static/js/data.js
+++ UFO-Level-2/StarterCode/static/js/data.js
@@ -7,6 +7,10 @@ var data = [{
     durationMinutes: "5 mins.",
     comments: "4 bright green circles high in the sky going in circles then one bright green light at my front door."
   },
+// Using the UFO dataset provided in the form of an array of JavaScript objects,
+//  write code that appends a table to your web page and then adds new rows of data 
+// for each UFO sighting.
+
   {
     datetime: "1/1/2010",
     city: "bonita",
